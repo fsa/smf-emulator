10b953c9730d64f49927fcb029a4b6eec9971ce1
Sergei Fedotov <fsa@tavda.info>
2023-01-12 23:16:49 +0700

    Форматирование Message.php

--- classes/Templates/Message.php
+++ classes/Templates/Message.php
@@ -1,37 +1,44 @@
 <?php
-namespace Templates;
-class Message {
-  public $title;
-  public $site_title;
-  public $style='default';
-  public $header;
-  public $message;
 
-  public function show() {
+namespace Templates;
+
+class Message
+{
+    public $title;
+    public $site_title;
+    public $style = 'default';
+    public $header;
+    public $message;
+
+    public function show()
+    {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 <meta charset="utf-8">
-<title><?=$this->title?></title>
-<link rel="stylesheet" type="text/css" href="/message.css">
+<title><?= $this->title ?></title>
+<link rel="stylesheet" type="text/css" href="/styles.css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<?=$this->header?>
+<?= $this->header ?>
 </head>
+
 <body bgcolor="white">
 <div class="container">
 <div class="page-header">
-  <h1><?=$this->site_title?></h1>
+<h1><?= $this->site_title ?></h1>
 </div>
-<div class="panel panel-<?=$this->style?>">
-  <div class="panel-heading"><?=$this->title?></div>
-  <div class="panel-body">
-<?=$this->message?>
-  </div>
-  </div>
+<div class="panel panel-<?= $this->style ?>">
+<div class="panel-heading"><?= $this->title ?></div>
+<div class="panel-body">
+<?= $this->message ?>
+</div>
+</div>
 </div>
 </body>
+
 </html>
 <?php
-  }
+    }
 }
